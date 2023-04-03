caa1dc2b66ea0f12f40f9d950e2a5187296c99f4
Anastasiia <schili@outlook.com>
2023-04-03 22:15:17 +0300

    add active page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,22 +1,30 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import css from './Layout.module.css'
 
+
+
 export const Layout = () => {
+    const location = useLocation();
+    const isActive = (path) => location.pathname === path;
     return (
-        <div>
-            <header>
-                <ul className={css.layoutList}>
-                    <li className={css.layoutItem}>
-                        <NavLink to='/' className={css.button} activeclassname={css.activeLink}><span className={css.linkContent}>Home</span></NavLink>
-                    </li>
-                    <li className={css.layoutItem}>
-                        <NavLink to='/movies' className={css.button} activeclassname={css.activeLink}><span className={css.linkContent}>Movies</span></NavLink>
-                    </li>
-                </ul>
-            </header>
-            <main>
-                <Outlet />
-            </main>
-        </div>
+      <div>
+        <header>
+          <ul className={css.layoutList}>
+            <li className={css.layoutItem}>
+              <Link to='/' className={css.button} style={isActive("/") ? {color: '#f92056'} : {}}>
+                <span className={css.linkContent}>Home</span>
+              </Link>
+            </li>
+            <li className={css.layoutItem}>
+              <Link to='/movies' className={css.button} style={isActive("/movies") ? {color: '#f92056'} : {}}>
+                <span className={css.linkContent}>Movies</span>
+              </Link>
+            </li>
+          </ul>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     );
-}
+  }
